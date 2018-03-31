ee37fde711f0e7bfffbcb459353ef3eaee2957a4
Tiana Mangual <tianamangual08@gmail.com>
2018-03-31 10:29:47 -0400

    Add files via upload

--- NBK/nbk_script.js
+++ NBK/nbk_script.js
@@ -1,9 +1,17 @@
-/* function toggleDiv() {
-  var x = document.getElementById("menu");
+function toggle() {
+    var x = document.getElementById("navbarNavAltMarkup");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
-*/
+
+function showGalleryMenu() {
+    var x = document.getElementById("galleryNav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
