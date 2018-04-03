522c16ed705da42e5e1b88331243e0f3d75f1fe8
Tiana Mangual <tianamangual08@gmail.com>
2018-04-03 12:42:30 -0400

    Add files via upload

--- NBK/nbk_script.js
+++ NBK/nbk_script.js
@@ -15,3 +15,103 @@ function showGalleryMenu() {
         x.style.display = "none";
     }
 }
+
+
+function changeText1() {
+    var x = document.getElementById("showMore1");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText2() {
+    var x = document.getElementById("showMore2");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText3() {
+    var x = document.getElementById("showMore3");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText4() {
+    var x = document.getElementById("showMore4");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText5() {
+    var x = document.getElementById("showMore5");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText6() {
+    var x = document.getElementById("showMore6");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText7() {
+    var x = document.getElementById("showMore7");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText8() {
+    var x = document.getElementById("showMore8");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText9() {
+    var x = document.getElementById("showMore9");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText10() {
+    var x = document.getElementById("showMore10");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
+
+function changeText11() {
+    var x = document.getElementById("showMore11");
+    if (x.innerHTML === "Show More...") {
+        x.innerHTML = "Show Less...";
+    } else {
+        x.innerHTML = "Show More...";
+    }
+}
