3a983cc9c9ae7141ffa9b1783cb7b4d144a9a82e
Tiana Mangual <tianamangual08@gmail.com>
2022-12-04 04:02:31 -0500

    Add files via upload

---
+++ LOMIV3/thl-collab-script.js
@@ -0,0 +1,148 @@
+/* Rotating Collab Images on Hover Start */
+var iconLinks = document.getElementsByClassName('collabLink');
+var iconImages = document.getElementsByClassName('collabIcon')
+
+iconLinks[0].addEventListener("mouseover", function() {
+  iconImages[0].style.transform = "rotate(20deg)";
+});
+
+iconLinks[0].addEventListener("mouseout", function() {
+  iconImages[0].style.transform = "rotate(50deg)";
+});
+
+iconLinks[1].addEventListener("mouseover", function() {
+  iconImages[1].style.transform = "rotate(20deg)";
+});
+
+iconLinks[1].addEventListener("mouseout", function() {
+  iconImages[1].style.transform = "rotate(50deg)";
+});
+
+iconLinks[2].addEventListener("mouseover", function() {
+  iconImages[2].style.transform = "rotate(20deg)";
+});
+
+iconLinks[2].addEventListener("mouseout", function() {
+  iconImages[2].style.transform = "rotate(50deg)";
+});
+
+iconLinks[3].addEventListener("mouseover", function() {
+  iconImages[3].style.transform = "rotate(20deg)";
+});
+
+iconLinks[3].addEventListener("mouseout", function() {
+  iconImages[3].style.transform = "rotate(50deg)";
+});
+
+iconLinks[4].addEventListener("mouseover", function() {
+  iconImages[4].style.transform = "rotate(20deg)";
+});
+
+iconLinks[4].addEventListener("mouseout", function() {
+  iconImages[4].style.transform = "rotate(50deg)";
+});
+
+iconLinks[5].addEventListener("mouseover", function() {
+  iconImages[5].style.transform = "rotate(20deg)";
+});
+
+iconLinks[5].addEventListener("mouseout", function() {
+  iconImages[5].style.transform = "rotate(50deg)";
+});
+
+iconLinks[6].addEventListener("mouseover", function() {
+  iconImages[6].style.transform = "rotate(20deg)";
+});
+
+iconLinks[6].addEventListener("mouseout", function() {
+  iconImages[6].style.transform = "rotate(50deg)";
+});
+
+iconLinks[7].addEventListener("mouseover", function() {
+  iconImages[7].style.transform = "rotate(20deg)";
+});
+
+iconLinks[7].addEventListener("mouseout", function() {
+  iconImages[7].style.transform = "rotate(50deg)";
+});
+
+iconLinks[8].addEventListener("mouseover", function() {
+  iconImages[8].style.transform = "rotate(20deg)";
+});
+
+iconLinks[8].addEventListener("mouseout", function() {
+  iconImages[8].style.transform = "rotate(50deg)";
+});
+
+iconLinks[9].addEventListener("mouseover", function() {
+  iconImages[9].style.transform = "rotate(20deg)";
+});
+
+iconLinks[9].addEventListener("mouseout", function() {
+  iconImages[9].style.transform = "rotate(50deg)";
+});
+
+iconLinks[10].addEventListener("mouseover", function() {
+  iconImages[10].style.transform = "rotate(20deg)";
+});
+
+iconLinks[10].addEventListener("mouseout", function() {
+  iconImages[10].style.transform = "rotate(50deg)";
+});
+
+iconLinks[11].addEventListener("mouseover", function() {
+  iconImages[11].style.transform = "rotate(20deg)";
+});
+
+iconLinks[11].addEventListener("mouseout", function() {
+  iconImages[11].style.transform = "rotate(50deg)";
+});
+
+iconLinks[12].addEventListener("mouseover", function() {
+  iconImages[12].style.transform = "rotate(20deg)";
+});
+
+iconLinks[12].addEventListener("mouseout", function() {
+  iconImages[12].style.transform = "rotate(50deg)";
+});
+
+iconLinks[13].addEventListener("mouseover", function() {
+  iconImages[13].style.transform = "rotate(20deg)";
+});
+
+iconLinks[13].addEventListener("mouseout", function() {
+  iconImages[13].style.transform = "rotate(50deg)";
+});
+
+iconLinks[14].addEventListener("mouseover", function() {
+  iconImages[14].style.transform = "rotate(20deg)";
+});
+
+iconLinks[14].addEventListener("mouseout", function() {
+  iconImages[14].style.transform = "rotate(50deg)";
+});
+
+iconLinks[15].addEventListener("mouseover", function() {
+  iconImages[15].style.transform = "rotate(20deg)";
+});
+
+iconLinks[15].addEventListener("mouseout", function() {
+  iconImages[15].style.transform = "rotate(50deg)";
+});
+
+iconLinks[16].addEventListener("mouseover", function() {
+  iconImages[16].style.transform = "rotate(20deg)";
+});
+
+iconLinks[16].addEventListener("mouseout", function() {
+  iconImages[16].style.transform = "rotate(50deg)";
+});
+
+iconLinks[17].addEventListener("mouseover", function() {
+  iconImages[17].style.transform = "rotate(20deg)";
+});
+
+iconLinks[17].addEventListener("mouseout", function() {
+  iconImages[17].style.transform = "rotate(50deg)";
+});
+/* Rotating Collab Images on Hover End */
