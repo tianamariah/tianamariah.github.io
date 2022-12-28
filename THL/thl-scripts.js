a8036f4c517efd7eadbcb51dbd496690792e0973
Tiana Mangual <tianamangual08@gmail.com>
2022-12-28 15:31:57 -0500

    Add files via upload

---
+++ THL/thl-scripts.js
@@ -0,0 +1,613 @@
+/* Hovering Over Homepage Images */
+const hpImages = document.getElementsByClassName('hpImages');
+const mobileImages = document.getElementsByClassName('mobileImages');
+const imageTitles = document.getElementsByClassName('imageTitles');
+const mobileImageTitles = document.getElementsByClassName('mobileImageTitles');
+
+mobileImages[0].addEventListener("mouseover", function () {
+  mobileImageTitles[0].style.visibility = "visible";
+});
+
+mobileImages[0].addEventListener("mouseout", function () {
+  mobileImageTitles[0].style.visibility = "hidden";
+});
+
+mobileImages[1].addEventListener("mouseover", function () {
+  mobileImageTitles[1].style.visibility = "visible";
+});
+
+mobileImages[1].addEventListener("mouseout", function () {
+  mobileImageTitles[1].style.visibility = "hidden";
+});
+
+mobileImages[2].addEventListener("mouseover", function () {
+  mobileImageTitles[2].style.visibility = "visible";
+});
+
+mobileImages[2].addEventListener("mouseout", function () {
+  mobileImageTitles[2].style.visibility = "hidden";
+});
+
+hpImages[0].addEventListener("mouseover", function () {
+  imageTitles[0].style.visibility = "visible";
+});
+
+hpImages[0].addEventListener("mouseout", function () {
+  imageTitles[0].style.visibility = "hidden";
+});
+
+hpImages[1].addEventListener("mouseover", function () {
+  imageTitles[1].style.visibility = "visible";
+});
+
+hpImages[1].addEventListener("mouseout", function () {
+  imageTitles[1].style.visibility = "hidden";
+});
+
+hpImages[2].addEventListener("mouseover", function () {
+  imageTitles[2].style.visibility = "visible";
+});
+
+hpImages[2].addEventListener("mouseout", function () {
+  imageTitles[2].style.visibility = "hidden";
+});
+
+hpImages[3].addEventListener("mouseover", function () {
+  imageTitles[3].style.visibility = "visible";
+});
+
+hpImages[3].addEventListener("mouseout", function () {
+  imageTitles[3].style.visibility = "hidden";
+});
+
+hpImages[4].addEventListener("mouseover", function () {
+  imageTitles[4].style.visibility = "visible";
+});
+
+hpImages[4].addEventListener("mouseout", function () {
+  imageTitles[4].style.visibility = "hidden";
+});
+
+hpImages[5].addEventListener("mouseover", function () {
+  imageTitles[5].style.visibility = "visible";
+});
+
+hpImages[5].addEventListener("mouseout", function () {
+  imageTitles[5].style.visibility = "hidden";
+});
+
+hpImages[6].addEventListener("mouseover", function () {
+  imageTitles[6].style.visibility = "visible";
+});
+
+hpImages[6].addEventListener("mouseout", function () {
+  imageTitles[6].style.visibility = "hidden";
+});
+
+hpImages[7].addEventListener("mouseover", function () {
+  imageTitles[7].style.visibility = "visible";
+});
+
+hpImages[7].addEventListener("mouseout", function () {
+  imageTitles[7].style.visibility = "hidden";
+});
+
+hpImages[8].addEventListener("mouseover", function () {
+  imageTitles[8].style.visibility = "visible";
+});
+
+hpImages[8].addEventListener("mouseout", function () {
+  imageTitles[8].style.visibility = "hidden";
+});
+
+hpImages[9].addEventListener("mouseover", function () {
+  imageTitles[9].style.visibility = "visible";
+});
+
+hpImages[9].addEventListener("mouseout", function () {
+  imageTitles[9].style.visibility = "hidden";
+});
+
+hpImages[10].addEventListener("mouseover", function () {
+  imageTitles[10].style.visibility = "visible";
+});
+
+hpImages[10].addEventListener("mouseout", function () {
+  imageTitles[10].style.visibility = "hidden";
+});
+
+hpImages[11].addEventListener("mouseover", function () {
+  imageTitles[11].style.visibility = "visible";
+});
+
+hpImages[11].addEventListener("mouseout", function () {
+  imageTitles[11].style.visibility = "hidden";
+});
+
+hpImages[12].addEventListener("mouseover", function () {
+  imageTitles[12].style.visibility = "visible";
+});
+
+hpImages[12].addEventListener("mouseout", function () {
+  imageTitles[12].style.visibility = "hidden";
+});
+
+hpImages[13].addEventListener("mouseover", function () {
+  imageTitles[13].style.visibility = "visible";
+});
+
+hpImages[13].addEventListener("mouseout", function () {
+  imageTitles[13].style.visibility = "hidden";
+});
+
+hpImages[14].addEventListener("mouseover", function () {
+  imageTitles[14].style.visibility = "visible";
+});
+
+hpImages[14].addEventListener("mouseout", function () {
+  imageTitles[14].style.visibility = "hidden";
+});
+
+hpImages[15].addEventListener("mouseover", function () {
+  imageTitles[15].style.visibility = "visible";
+});
+
+hpImages[15].addEventListener("mouseout", function () {
+  imageTitles[15].style.visibility = "hidden";
+});
+
+hpImages[16].addEventListener("mouseover", function () {
+  imageTitles[16].style.visibility = "visible";
+});
+
+hpImages[16].addEventListener("mouseout", function () {
+  imageTitles[16].style.visibility = "hidden";
+});
+
+hpImages[17].addEventListener("mouseover", function () {
+  imageTitles[17].style.visibility = "visible";
+});
+
+hpImages[17].addEventListener("mouseout", function () {
+  imageTitles[17].style.visibility = "hidden";
+});
+
+hpImages[18].addEventListener("mouseover", function () {
+  imageTitles[18].style.visibility = "visible";
+});
+
+hpImages[18].addEventListener("mouseout", function () {
+  imageTitles[18].style.visibility = "hidden";
+});
+
+hpImages[19].addEventListener("mouseover", function () {
+  imageTitles[19].style.visibility = "visible";
+});
+
+hpImages[19].addEventListener("mouseout", function () {
+  imageTitles[19].style.visibility = "hidden";
+});
+
+hpImages[20].addEventListener("mouseover", function () {
+  imageTitles[20].style.visibility = "visible";
+});
+
+hpImages[20].addEventListener("mouseout", function () {
+  imageTitles[20].style.visibility = "hidden";
+});
+
+hpImages[21].addEventListener("mouseover", function () {
+  imageTitles[21].style.visibility = "visible";
+});
+
+hpImages[21].addEventListener("mouseout", function () {
+  imageTitles[21].style.visibility = "hidden";
+});
+
+hpImages[22].addEventListener("mouseover", function () {
+  imageTitles[22].style.visibility = "visible";
+});
+
+hpImages[22].addEventListener("mouseout", function () {
+  imageTitles[22].style.visibility = "hidden";
+});
+
+hpImages[23].addEventListener("mouseover", function () {
+  imageTitles[23].style.visibility = "visible";
+});
+
+hpImages[23].addEventListener("mouseout", function () {
+  imageTitles[23].style.visibility = "hidden";
+});
+
+hpImages[24].addEventListener("mouseover", function () {
+  imageTitles[24].style.visibility = "visible";
+});
+
+hpImages[24].addEventListener("mouseout", function () {
+  imageTitles[24].style.visibility = "hidden";
+});
+
+hpImages[25].addEventListener("mouseover", function () {
+  imageTitles[25].style.visibility = "visible";
+});
+
+hpImages[25].addEventListener("mouseout", function () {
+  imageTitles[25].style.visibility = "hidden";
+});
+
+hpImages[26].addEventListener("mouseover", function () {
+  imageTitles[26].style.visibility = "visible";
+});
+
+hpImages[26].addEventListener("mouseout", function () {
+  imageTitles[26].style.visibility = "hidden";
+});
+
+hpImages[27].addEventListener("mouseover", function () {
+  imageTitles[27].style.visibility = "visible";
+});
+
+hpImages[27].addEventListener("mouseout", function () {
+  imageTitles[27].style.visibility = "hidden";
+});
+
+hpImages[28].addEventListener("mouseover", function () {
+  imageTitles[28].style.visibility = "visible";
+});
+
+hpImages[28].addEventListener("mouseout", function () {
+  imageTitles[28].style.visibility = "hidden";
+});
+
+hpImages[29].addEventListener("mouseover", function () {
+  imageTitles[29].style.visibility = "visible";
+});
+
+hpImages[29].addEventListener("mouseout", function () {
+  imageTitles[29].style.visibility = "hidden";
+});
+
+hpImages[30].addEventListener("mouseover", function () {
+  imageTitles[30].style.visibility = "visible";
+});
+
+hpImages[30].addEventListener("mouseout", function () {
+  imageTitles[30].style.visibility = "hidden";
+});
+
+hpImages[31].addEventListener("mouseover", function () {
+  imageTitles[31].style.visibility = "visible";
+});
+
+hpImages[31].addEventListener("mouseout", function () {
+  imageTitles[31].style.visibility = "hidden";
+});
+/* Hovering Over Homepage Images */
+
+
+/* Hover for Nav Items */
+const navLinks = document.getElementsByClassName('navLinks');
+const navToolTips = document.getElementsByClassName('tooltiptext');
+const bis = document.getElementsByClassName('bi');
+
+navLinks[0].addEventListener("mouseover", function () {
+  bis[0].style.color = "#463f3a";
+  bis[0].style.fontWeight = "700";
+  navToolTips[0].style.color = "#463f3a";
+  navToolTips[0].style.fontWeight = "900";
+});
+
+navLinks[0].addEventListener("mouseout", function () {
+  bis[0].style.color = "#8a817c";
+  bis[0].style.fontWeight = "700";
+  navToolTips[0].style.color = "#8a817c";
+  navToolTips[0].style.fontWeight = "400";
+});
+
+navLinks[1].addEventListener("mouseover", function () {
+  bis[1].style.color = "#463f3a";
+  bis[1].style.fontWeight = "700";
+  navToolTips[1].style.color = "#463f3a";
+  navToolTips[1].style.fontWeight = "900";
+});
+
+navLinks[1].addEventListener("mouseout", function () {
+  bis[1].style.color = "#8a817c";
+  bis[1].style.fontWeight = "700";
+  navToolTips[1].style.color = "#8a817c";
+  navToolTips[1].style.fontWeight = "400";
+});
+
+navLinks[2].addEventListener("mouseover", function () {
+  bis[2].style.color = "#463f3a";
+  bis[2].style.fontWeight = "700";
+  navToolTips[2].style.color = "#463f3a";
+  navToolTips[2].style.fontWeight = "900";
+});
+
+navLinks[2].addEventListener("mouseout", function () {
+  bis[2].style.color = "#8a817c";
+  bis[2].style.fontWeight = "700";
+  navToolTips[2].style.color = "#8a817c";
+  navToolTips[2].style.fontWeight = "400";
+});
+
+navLinks[3].addEventListener("mouseover", function () {
+  bis[3].style.color = "#463f3a";
+  bis[3].style.fontWeight = "700";
+  navToolTips[3].style.color = "#463f3a";
+  navToolTips[3].style.fontWeight = "900";
+});
+
+navLinks[3].addEventListener("mouseout", function () {
+  bis[3].style.color = "#8a817c";
+  bis[3].style.fontWeight = "700";
+  navToolTips[3].style.color = "#8a817c";
+  navToolTips[3].style.fontWeight = "400";
+});
+
+navLinks[4].addEventListener("mouseover", function () {
+  bis[4].style.color = "#463f3a";
+  bis[4].style.fontWeight = "700";
+  navToolTips[4].style.color = "#463f3a";
+  navToolTips[4].style.fontWeight = "900";
+});
+
+navLinks[4].addEventListener("mouseout", function () {
+  bis[4].style.color = "#8a817c";
+  bis[4].style.fontWeight = "700";
+  navToolTips[4].style.color = "#8a817c";
+  navToolTips[4].style.fontWeight = "400";
+});
+
+navLinks[5].addEventListener("mouseover", function () {
+  bis[5].style.color = "#463f3a";
+  bis[5].style.fontWeight = "700";
+  navToolTips[5].style.color = "#463f3a";
+  navToolTips[5].style.fontWeight = "900";
+});
+
+navLinks[5].addEventListener("mouseout", function () {
+  bis[5].style.color = "#8a817c";
+  bis[5].style.fontWeight = "700";
+  navToolTips[5].style.color = "#8a817c";
+  navToolTips[5].style.fontWeight = "400";
+});
+
+navLinks[6].addEventListener("mouseover", function () {
+  bis[6].style.color = "#463f3a";
+  bis[6].style.fontWeight = "700";
+  navToolTips[6].style.color = "#463f3a";
+  navToolTips[6].style.fontWeight = "900";
+});
+
+navLinks[6].addEventListener("mouseout", function () {
+  bis[6].style.color = "#8a817c";
+  bis[6].style.fontWeight = "700";
+  navToolTips[6].style.color = "#8a817c";
+  navToolTips[6].style.fontWeight = "400";
+});
+
+navLinks[7].addEventListener("mouseover", function () {
+  bis[7].style.color = "#463f3a";
+  bis[7].style.fontWeight = "700";
+  navToolTips[7].style.color = "#463f3a";
+  navToolTips[7].style.fontWeight = "900";
+});
+
+navLinks[7].addEventListener("mouseout", function () {
+  bis[7].style.color = "#8a817c";
+  bis[7].style.fontWeight = "700";
+  navToolTips[7].style.color = "#8a817c";
+  navToolTips[7].style.fontWeight = "400";
+});
+
+navLinks[8].addEventListener("mouseover", function () {
+  bis[8].style.color = "#463f3a";
+  bis[8].style.fontWeight = "700";
+  navToolTips[8].style.color = "#463f3a";
+  navToolTips[8].style.fontWeight = "900";
+});
+
+navLinks[8].addEventListener("mouseout", function () {
+  bis[8].style.color = "#8a817c";
+  bis[8].style.fontWeight = "700";
+  navToolTips[8].style.color = "#8a817c";
+  navToolTips[8].style.fontWeight = "400";
+});
+
+navLinks[9].addEventListener("mouseover", function () {
+  bis[9].style.color = "#463f3a";
+  bis[9].style.fontWeight = "700";
+  navToolTips[9].style.color = "#463f3a";
+  navToolTips[9].style.fontWeight = "900";
+});
+
+navLinks[9].addEventListener("mouseout", function () {
+  bis[9].style.color = "#8a817c";
+  bis[9].style.fontWeight = "700";
+  navToolTips[9].style.color = "#8a817c";
+  navToolTips[9].style.fontWeight = "400";
+});
+
+navLinks[10].addEventListener("mouseover", function () {
+  bis[10].style.color = "#463f3a";
+  bis[10].style.fontWeight = "700";
+  navToolTips[10].style.color = "#463f3a";
+  navToolTips[10].style.fontWeight = "900";
+});
+
+navLinks[10].addEventListener("mouseout", function () {
+  bis[10].style.color = "#8a817c";
+  bis[10].style.fontWeight = "700";
+  navToolTips[10].style.color = "#8a817c";
+  navToolTips[10].style.fontWeight = "400";
+});
+
+navLinks[11].addEventListener("mouseover", function () {
+  bis[11].style.color = "#463f3a";
+  bis[11].style.fontWeight = "700";
+  navToolTips[11].style.color = "#463f3a";
+  navToolTips[11].style.fontWeight = "900";
+});
+
+navLinks[11].addEventListener("mouseout", function () {
+  bis[11].style.color = "#8a817c";
+  bis[11].style.fontWeight = "700";
+  navToolTips[11].style.color = "#8a817c";
+  navToolTips[11].style.fontWeight = "400";
+});
+
+navLinks[12].addEventListener("mouseover", function () {
+  bis[12].style.color = "#463f3a";
+  bis[12].style.fontWeight = "700";
+  navToolTips[12].style.color = "#463f3a";
+  navToolTips[12].style.fontWeight = "900";
+});
+
+navLinks[12].addEventListener("mouseout", function () {
+  bis[12].style.color = "#8a817c";
+  bis[12].style.fontWeight = "700";
+  navToolTips[12].style.color = "#8a817c";
+  navToolTips[12].style.fontWeight = "400";
+});
+
+navLinks[13].addEventListener("mouseover", function () {
+  bis[13].style.color = "#463f3a";
+  bis[13].style.fontWeight = "700";
+  navToolTips[13].style.color = "#463f3a";
+  navToolTips[13].style.fontWeight = "900";
+});
+
+navLinks[13].addEventListener("mouseout", function () {
+  bis[13].style.color = "#8a817c";
+  bis[13].style.fontWeight = "700";
+  navToolTips[13].style.color = "#8a817c";
+  navToolTips[13].style.fontWeight = "400";
+});
+
+navLinks[14].addEventListener("mouseover", function () {
+  bis[14].style.color = "#463f3a";
+  bis[14].style.fontWeight = "700";
+  navToolTips[14].style.color = "#463f3a";
+  navToolTips[14].style.fontWeight = "900";
+});
+
+navLinks[14].addEventListener("mouseout", function () {
+  bis[14].style.color = "#8a817c";
+  bis[14].style.fontWeight = "700";
+  navToolTips[14].style.color = "#8a817c";
+  navToolTips[14].style.fontWeight = "400";
+});
+
+navLinks[15].addEventListener("mouseover", function () {
+  bis[15].style.color = "#463f3a";
+  bis[15].style.fontWeight = "700";
+  navToolTips[15].style.color = "#463f3a";
+  navToolTips[15].style.fontWeight = "900";
+});
+
+navLinks[15].addEventListener("mouseout", function () {
+  bis[15].style.color = "#8a817c";
+  bis[15].style.fontWeight = "700";
+  navToolTips[15].style.color = "#8a817c";
+  navToolTips[15].style.fontWeight = "400";
+});
+/* Hover for Nav Items */
+
+/* Clicking on PI Vids*/
+const radioBtns = document.getElementsByName('vidCarouselRB');
+const piVideos = document.getElementsByClassName('piVideos');
+
+//Adding Cursor on Hover
+radioBtns[0].addEventListener("mouseover", function () {
+  radioBtns[0].style.cursor = "pointer";
+});
+
+radioBtns[1].addEventListener("mouseover", function () {
+  radioBtns[1].style.cursor = "pointer";
+});
+
+radioBtns[2].addEventListener("mouseover", function () {
+  radioBtns[2].style.cursor = "pointer";
+});
+
+radioBtns[3].addEventListener("mouseover", function () {
+  radioBtns[3].style.cursor = "pointer";
+});
+
+radioBtns[4].addEventListener("mouseover", function () {
+  radioBtns[4].style.cursor = "pointer";
+});
+
+//Clicking on Radio to Disply Next Vid
+radioBtns[0].addEventListener("click", function () {
+  piVideos[0].style.display = "block";
+
+
+  piVideos[1].style.display = "none";
+  piVideos[2].style.display = "none";
+  piVideos[3].style.display = "none";
+  piVideos[4].style.display = "none";
+  piVideos[5].style.display = "none";
+});
+
+radioBtns[1].addEventListener("click", function () {
+  piVideos[1].style.display = "block";
+
+  piVideos[0].style.display = "none";
+  piVideos[2].style.display = "none";
+  piVideos[3].style.display = "none";
+  piVideos[4].style.display = "none";
+  piVideos[5].style.display = "none";
+});
+
+radioBtns[2].addEventListener("click", function () {
+  piVideos[2].style.display = "block";
+
+  piVideos[0].style.display = "none";
+  piVideos[1].style.display = "none";
+  piVideos[3].style.display = "none";
+  piVideos[4].style.display = "none";
+  piVideos[5].style.display = "none";
+});
+
+radioBtns[3].addEventListener("click", function () {
+  piVideos[3].style.display = "block";
+
+  piVideos[0].style.display = "none";
+  piVideos[1].style.display = "none";
+  piVideos[2].style.display = "none";
+  piVideos[4].style.display = "none";
+  piVideos[5].style.display = "none";
+});
+
+radioBtns[4].addEventListener("click", function () {
+  piVideos[4].style.display = "block";
+
+  piVideos[0].style.display = "none";
+  piVideos[1].style.display = "none";
+  piVideos[2].style.display = "none";
+  piVideos[3].style.display = "none";
+  piVideos[5].style.display = "none";
+});
+
+radioBtns[5].addEventListener("click", function () {
+  piVideos[5].style.display = "block";
+
+  piVideos[0].style.display = "none";
+  piVideos[1].style.display = "none";
+  piVideos[2].style.display = "none";
+  piVideos[3].style.display = "none";
+  piVideos[4].style.display = "none";
+});
+/*Clicking on PI Vids */
+
+/* Hiding and Showing Divs in Intro Section */
+function replace(hide, show) {
+  document.getElementById(hide).style.display = "none";
+  document.getElementById(show).style.display = "block";
+};
+/* Hiding and Showing Divs in Intro Section End */
+
---
+++ THL/thl-team-script.js
@@ -0,0 +1,144 @@
+/* Showing + Closing Bios */
+document.getElementById("overLayOne").addEventListener("click", function() {
+  const getModalBox =document.getElementsByClassName("modalBoxOne");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeOne").addEventListener("click", function() {
+  const getModalBox =document.getElementsByClassName("modalBoxOne");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayTwo").addEventListener("click", function() {
+  const getModalBox =document.getElementsByClassName("modalBoxTwo");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeTwo").addEventListener("click", function() {
+  const getModalBox =document.getElementsByClassName("modalBoxTwo");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayThree").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxThree");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeThree").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxThree");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayFour").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxFour");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeFour").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxFour");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayFive").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxFive");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeFive").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxFive");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLaySix").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxSix");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeSix").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxSix");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLaySeven").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxSeven");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeSeven").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxSeven");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayEight").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxEight");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeEight").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxEight");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayNine").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxNine");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeNine").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxNine");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayTen").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxTen");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeTen").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxTen");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayEleven").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxEleven");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeEleven").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxEleven");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayTwelve").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxTwelve");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeTwelve").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxTwelve");
+  getModalBox[0].style.display= 'none';
+
+});
+
+document.getElementById("overLayThirteen").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxThirteen");
+  getModalBox[0].style.display = 'block';
+});
+
+document.getElementById("closeThirteen").addEventListener("click", function() {
+  const getModalBox = document.getElementsByClassName("modalBoxThirteen");
+  getModalBox[0].style.display= 'none';
+
+});
+/* Showing + Closing Bios End */
